5d585193a0c013d51e91057c44577a465f0273f9
nkkumar477 <59718776+nkkumar477@users.noreply.github.com>
2021-05-18 07:01:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# graminmantra
+ Graminmantra Project
---
+++ gramin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GraminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gramin'
